8ace58c4c8f4cd904367017a2d380fab3d567f98
Mishutka007 <serkov.mishaPVZ@yandex.ru>
2023-10-10 22:37:44 +0300

    fix

--- calculator.js
+++ calculator.js
@@ -1,17 +1,48 @@
 let result = document.getElementById("zzzz");
+let qrrentSine = null;
 let calculate = (number) => {
   if (result.value.length === 1 && number === ".") {
     number = "0.";
+  }
+  if (result.value.length === 1 && ["+", "-", "/", "*"].includes(number)) {
+    return;
+  }
+  if (!qrrentSine && ["+", "-", "/", "*"].includes(number)) {
+    qrrentSine = number;
+    return;
+  }
+  if (number == ".") {
+    if (check("+") || check("-") || check("/") || check("*")) {
+      const numbers = result.value.split(qrrentSine);
+      if (numbers[1].includes(".")) {
+        return;
+      }
+    } else {
+      if (result.value.includes(".")) {
+        return;
+      }
+    }
+  }
+  if (
+    (check("+") || check("-") || check("/") || check("*")) &&
+    ["+", "-", "/", "*"].includes(number)
+  ) {
+    return;
   }
   result.value += number;
 };
 let SUM = () => {
   try {
     result.value = eval(result.value);
+    qrrentSine = null;
   } catch (err) {
     alert("Передумай!");
   }
 };
 function del() {
   result.value = " ";
+  qrrentSine = null;
+}
+function check(param) {
+  return result.value.includes(param);
 }
